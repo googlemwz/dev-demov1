2208f32ee3bf657afbd28001f62de12c6642962c
wenzhong.ma <403805265@qq.com>
2020-08-17 20:18:01 +0800

    update 修改

--- tools-demov1/src/main/java/com/mwz/test/DemoTest.java
+++ tools-demov1/src/main/java/com/mwz/test/DemoTest.java
@@ -10,5 +10,12 @@ public class DemoTest {
 
     public static void main(String[] args) {
         System.out.println("系统测试输入！");
+        DemoTest demoTest = new DemoTest();
+        demoTest.println();
+    }
+
+
+    public void println(){
+        System.out.println("测试打印方法【print】");
     }
 }
