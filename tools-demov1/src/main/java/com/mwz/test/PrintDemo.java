a605210ef509d58ba4c6ed74cc86f8c1fb37acba
wenzhong.ma <403805265@qq.com>
2020-08-17 20:45:43 +0800

    add one class file

---
+++ tools-demov1/src/main/java/com/mwz/test/PrintDemo.java
@@ -0,0 +1,20 @@
+package com.mwz.test;
+
+/**
+ * @description:
+ * @author: Wenzhong.Ma
+ * @create: 2020-08-17 20:42
+ **/
+
+public class PrintDemo {
+
+
+    public static void main(String[] args) {
+        //1.1 初始化数据
+        System.out.println("1.1 初始化数据 ");
+        //1.2 系统名称
+        System.out.println("1.2 系统名称");
+        //1.3 我们的名称
+        System.out.println("1.3 我们的名称 ");
+    }
+}
