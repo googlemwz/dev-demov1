bc24184c9f6b169c351e5f2f33d3faf3583f45ca
wenzhong.ma <403805265@qq.com>
2020-08-17 19:56:14 +0800

    init test

---
+++ tools-demov1/src/main/java/com/mwz/test/DemoTest.java
@@ -0,0 +1,14 @@
+package com.mwz.test;
+
+/**
+ * @description:
+ * @author: Wenzhong.Ma
+ * @create: 2020-08-17 19:51
+ **/
+
+public class DemoTest {
+
+    public static void main(String[] args) {
+        System.out.println("系统测试输入！");
+    }
+}
