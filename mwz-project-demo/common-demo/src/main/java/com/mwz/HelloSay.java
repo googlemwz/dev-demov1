d09c52b71ec261973e434cae11b62aaca559a4ba
wenzhong.ma <403805265@qq.com>
2021-03-07 14:20:28 +0800

    多模块项目

---
+++ mwz-project-demo/common-demo/src/main/java/com/mwz/HelloSay.java
@@ -0,0 +1,14 @@
+package com.mwz;
+
+/**
+ * @description:
+ * @author: Wenzhong.Ma
+ * @create: 2021-03-07 14:08
+ **/
+
+public class HelloSay {
+
+    public String Say( String name ){
+        return "Hello "+name;
+    }
+}
---
+++ mwz-project-demo/test-demo/src/main/java/com/mwz/App.java
@@ -0,0 +1,15 @@
+package com.mwz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        HelloSay helloSay = new HelloSay();
+        System.out.println( helloSay.Say("mawenzhong") );
+    }
+}
